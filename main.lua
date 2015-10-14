2f68b4f1a8c6a4e697438b3e508d1712b7651618
tonibm19 <tonibm19@gmail.com>
2015-10-14 20:40:38 +0200

    no more crashes
    :D

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-Time = 0
+Time = {}
 
 function Initialize(Plugin)
 	Plugin:SetName("NoCheat")
 	Plugin:SetVersion(3)
 
-	cPluginManager.AddHook(cPluginManager.HOOK_TICK, OnTick)
+	cPluginManager.AddHook(cPluginManager.HOOK_WORLD_TICK, OnWorldTick)
 
 	LOG("Initialized " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
 	return true
@@ -14,24 +14,32 @@ end
 
 
 
-function OnTick(TimeDelta)
-    print(Time)
-    if Time == 20 then
-        Time = 0
+function OnWorldTick(World, TimeDelta)
+    if Time[World:GetName()] == 20 then
+        Time[World:GetName()] = 0
         local ForEachPlayer = function(Player)
-            World = Player:GetWorld()
-            x = Player:GetPosX()
+			x = Player:GetPosX()
             y = Player:GetPosY()
             z = Player:GetPosZ()
-            if World:GetBlock(x, y, z) == E_BLOCK_AIR and World:GetBlock(x, y - 1, z) == E_BLOCK_AIR and World:GetBlock(x, y - 2, z) == E_BLOCK_AIR and World:GetBlock(x, y - 3, z) == E_BLOCK_AIR then
+            if lasty == nil then
+				lasty = Player:GetPosY()
+			end
+            World = Player:GetWorld()
+            if World:GetBlock(x, y - 1, z) == E_BLOCK_AIR and World:GetBlock(x, y - 2, z) == E_BLOCK_AIR and World:GetBlock(x, y - 3, z) == E_BLOCK_AIR and y >= lasty then
                 Player:SendMessage("Yoou're flying")
             end
+			lastx = Player:GetPosX()
+            lasty = Player:GetPosY()
+            lastz = Player:GetPosZ()
         end
         local ForEachWorld = function(World)
             World:ForEachPlayer(ForEachPlayer)
         end
         cRoot:Get():ForEachWorld(ForEachWorld)
+	elseif Time[World:GetName()] == nil then
+		Time[World:GetName()] = 0
     else
-        Time = Time + 1
+		Time[World:GetName()] = Time[World:GetName()] + 1
     end
 end
+
